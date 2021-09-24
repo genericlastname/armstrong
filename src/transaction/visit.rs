75e62dd701681d0697a6564603e5a515b4b9d664
Kyle Brown <brownkylej@gmail.com>
2021-09-24 11:53:58 -0400

    Dummy out visit()

--- src/transaction/visit.rs
+++ src/transaction/visit.rs
@@ -1,12 +1,18 @@
-use std::io::{Read, Write};
-use std::net::TcpStream;
+// use std::io::{Read, Write};
+// use std::net::TcpStream;
 
 use crate::transaction::response::Response;
 
 // Visits the specified url at the given port and returns the resulting
 // Response.
 pub fn visit(scheme: &str, address: &str, port: &str, path: &str) -> Response {
-    let for_tcp = format!("{}:{}", address, port);
-    let for_dns = format!("{}", address);
-    let request = format!("{}://{}:{}/{}\r\n", scheme, address, port, path);
+    let _for_tcp = format!("{}:{}", address, port);
+    let _for_dns = format!("{}", address);
+    let _request = format!("{}://{}:{}/{}\r\n", scheme, address, port, path);
+    Response {
+        status: 255,
+        mimetype: "Dummy mimetype".to_owned(),
+        charset: "Dummy charset".to_owned(),
+        body: "Dummy body".to_owned(),
+    }
 }
