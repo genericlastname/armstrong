6539c292645f58875999922ad0bae3b616681ab4
Kyle Brown <brownkylej@gmail.com>
2021-10-13 14:23:43 -0400

    Reset main.rs, update test, clean up comments

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-// use gemtext::parse_gemtext;
-// use transaction::response::Response;
-// use transaction::visit::visit;
-
 pub mod gemtext;
 
 pub mod transaction {
@@ -11,13 +7,5 @@ pub mod transaction {
 }
 
 fn main() {
-    // println!("Hello I'm not setup yet, try running `cargo test`");
-    let r: transaction::response::Response = transaction::visit::visit(
-        "gemini",
-        "carcosa.net",
-        "1965",
-        "");
-    println!("{}", r.status);
-    println!("{}", r.mimetype);
-    println!("{}", r.body);
+    println!("Hello I'm not setup yet, try running `cargo test`");
 }
--- src/transaction/visit.rs
+++ src/transaction/visit.rs
@@ -10,7 +10,6 @@ use crate::transaction::dummy_verifier::DummyVerifier;
 // Response.
 pub fn visit(scheme: &str, address: &str, port: &str, path: &str) -> Response {
     let for_tcp = format!("{}:{}", address, port);
-    // let for_dns = format!("{}", address);
     let request = format!("{}://{}:{}/{}\r\n", scheme, address, port, path);
 
     // TLS stuff.
@@ -55,17 +54,7 @@ pub fn visit(scheme: &str, address: &str, port: &str, path: &str) -> Response {
     }
     let _ = client.reader().read_to_end(&mut data);
     let content = String::from_utf8_lossy(&data).to_string();
-    // println!("data: {}", header);
 
-    // Get body
-    // client.read_tls(&mut socket).unwrap();
-    // client.process_new_packets().unwrap();
-    // data = Vec::new();
-    // let _ = client.reader().read_to_end(&mut data);
-    // let body = String::from_utf8_lossy(&data).to_string();
-    // println!("body: {}", body);
-
-    // Response::new(&header, &body)
     Response::new(&content)
 }
 
@@ -74,11 +63,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn visit_returns_dummy_response() {
-        let response: Response = visit("", "", "", "");
-        assert_eq!(response.status, 255);
-        assert_eq!(response.mimetype, "Dummy mimetype");
-        assert_eq!(response.charset, "Dummy charset");
-        assert_eq!(response.body, "Dummy body");
+    fn visit_to_valid_site_returns_ok_status() {
+        let response: Response = visit(
+            "gemini",
+            "carcosa.net",
+            "1965",
+            "");
+        assert_eq!(response.status, 20);
+        assert_eq!(response.mimetype, "text/gemini");
+        assert_eq!(response.charset, "utf-8");
     }
 }
