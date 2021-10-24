db93f6ba57c6096f8d6b8c98266e01f409ad4107
Kyle Brown <brownkylej@gmail.com>
2021-10-24 19:20:36 -0400

    Move settings

--- src/settings/constants.rs
+++
@@ -1,7 +0,0 @@
-// The default armstrong config file. This file is copied to
-// $XDG_HOME_CONFIG/armstrong/config.toml if the file does not already exist or
-// $HOME/.config/armstrong/config.toml if $XDG_HOME_CONFIG is unset.
-pub const DEFAULT_CONFIG_TOML: &str = r#"
-[downloads]
-download_dir = "$HOME/Downloads/"
-"#;
