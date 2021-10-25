85d2b5bb5ec4578d2da476db8941efd492315c5a
Kyle Brown <brownkylej@gmail.com>
2021-10-24 21:36:18 -0400

    Rudimentary UI

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ fn main() {
     // println!("Hello I'm not setup yet, try running `cargo test`");
     let mut app = Cursive::new();
     ui::tui::configure_callbacks(&mut app);
-    ui::tui::configure_default_view(&mut app);
+    let r = transaction::visit::visit("gemini", "carcosa.net", "1965", "");
+    ui::tui::client_screen(&mut app, &r);
     app.run();
 }
--- src/ui/tui.rs
+++ src/ui/tui.rs
@@ -1,32 +1,19 @@
 use cursive::Cursive;
-use cursive::view::{Nameable, Resizable};
-use cursive::views::{Dialog, LinearLayout, TextView};
+use cursive::view::{Nameable, Resizable, Scrollable};
+use cursive::views::{BoxView, Dialog, LinearLayout, TextView, ScrollView};
+
+use crate::transaction::response::Response;
+use crate::transaction::visit::visit;
 
 pub fn configure_callbacks(app: &mut Cursive) {
     app.add_global_callback('q', |s| s.quit());
 }
 
-pub fn configure_default_view(app: &mut Cursive) {
-    app.add_layer(Dialog::text("This is a survey\nPress <Next> when ready.")
-        .title("Important")
-        .button("Next", show_next));
+pub fn client_screen(
+    s: &mut Cursive,
+    response: &Response,
+) {
+    let text_area = ScrollView::new(TextView::new(&response.body));
+    s.add_layer(LinearLayout::vertical()
+        .child(text_area));
 }
-
-fn show_next(app: &mut Cursive) {
-    app.pop_layer();
-    app.add_layer(Dialog::text("The thing")
-        .title("Question")
-        .button("Yes!", |s| show_answer(s, "I knew it! Well done!"))
-        .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
-        .button("Uh?", |s| s.add_layer(Dialog::info("Try again!"))));
-}
-
-fn show_answer(s: &mut Cursive, msg: &str) {
-    s.pop_layer();
-    s.add_layer(Dialog::text(msg)
-        .title("Results")
-        .button("Finish", |s| s.quit()));
-}
-
-// Is triggered when config file is not found, obstensibly on a fresh install.
-
