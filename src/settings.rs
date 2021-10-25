e0b093dd373ed29f4bc99541ace75e7a67437193
Kyle Brown <brownkylej@gmail.com>
2021-10-24 20:35:28 -0400

    Add test for config creation

--- src/settings.rs
+++ src/settings.rs
@@ -1,36 +1,49 @@
+// The default armstrong config file. This file is copied to
+// $XDG_HOME_CONFIG/armstrong/config.toml if the file does not already exist or
+// $HOME/.config/armstrong/config.toml if $XDG_HOME_CONFIG is unset.
+const DEFAULT_CONFIG_TOML: &str = r#"
+[downloads]
+download_dir = "$HOME/Downloads/"
+"#;
+
 use std::env;
 use std::fs::File;
+use std::io::Write;
 use std::path::Path;
 
 pub fn create_config_file(override_path: &str) {
-    let mut config_path: &str;
+    let config_path: &Path;
+    let config_path_string: String;
 
     if override_path.is_empty() {
-        let home_dir = env::var("HOME") {
-            Ok(h) => h.to_owned(),
-            Err(e) => return Err(e),
-        }
-
-        let config_dir = match env::var("XDG_HOME_CONFIG") {
-            Ok(c) => c.to_owned(),
-            Err(e) => format!("{}/.config", home_dir),
-        }
-
-        config_path = Path::new(format!("{}/armstrong/config.toml", config_dir));
+        let home_dir = env::var("HOME").unwrap();
+        config_path_string = format!("{}/.config/armstrong/config.toml", 
+                                    home_dir);
+        config_path = Path::new(&config_path_string);
     } else {
         config_path = Path::new(override_path);
     }
 
-    let mut file = match File::create(&config_path) {
-        Ok(file) => file,
-        Err(e) => panic!("Couldn't create {}: {}",
-                    config_path.display(),
-                    e),
-    }
-    match file.write_all(settings::constants::DEFAULT_CONFIG_TOML.as_bytes()) {
+    let mut file = File::create(&config_path).unwrap();
+    match file.write(DEFAULT_CONFIG_TOML.as_bytes()) {
         Ok(_) => (),
         Err(e) => panic!("Couldn't write to {}: {}",
                     config_path.display(),
                     e),
     }                    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Read;
+
+    #[test]
+    fn create_config_file_works() {
+        create_config_file("/tmp/config.toml");
+        let mut config = File::open("/tmp/config.toml").unwrap();
+        let mut s = String::new();
+        config.read_to_string(&mut s).expect("Couldn't open file.");
+        assert_eq!(s, DEFAULT_CONFIG_TOML);
+    }
 }
